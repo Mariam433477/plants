d5c17b7a7a5f7100bf5e6af0982fe63914e0d6cc
Mariam433477 <mariam.ehabsarhan@gmail.com>
2025-10-02 00:07:36 +0300

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
- 
-import { createRoot } from 'react-dom/client'
- 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
-import MainLayout from './layout/MainLayout';
-import style from './css/style.module.css'
-  import '../src/css/navbar.css'
-createRoot(document.getElementById('root')).render(
- 
-   <MainLayout/>
-  
-)
+import { createRoot } from "react-dom/client";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import MainLayout from "./layout/MainLayout";
+import "./css/style.css";
+import "../src/css/navbar.css";
+createRoot(document.getElementById("root")).render(<MainLayout />);
